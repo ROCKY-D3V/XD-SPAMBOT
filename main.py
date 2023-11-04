a54df8c9e3ce698b2e28c7febfd225692620c650
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-11-04 09:40:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("\nOPSHIVAM 𝐒𝐩𝐚𝐦 𝐁𝐨𝐭𝐬 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 😎🤘🏻\nMy Master ---> @itsz_shivam")
+print("\nʀᴏᴄᴋʏ sᴘᴀᴍ ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟ 😎🤘🏻\nɱყ ɱαʂƚҽɾ ---> @ROCKY_ISS_BACK")
 
 
 async def main():
